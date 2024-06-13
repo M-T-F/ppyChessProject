dac1aa1f16b06d353afbc03185441332bedb19a3
M-T-F <s27832@pjwstk.edu.pl>
2024-06-14 00:36:05 +0200

    bazeView

--- Board.py
+++ Board.py
@@ -7,16 +7,23 @@ from Field import Field
 
 class Board:
     def __init__(self):
-        mac = np.array([[Field(x, y) for x in range(0, 8)] for y in range(0, 8)])
-        self.board = pd.DataFrame(mac,["a", "b", "c", "d", "e", "f", "g", "h"])
+        mac = np.array([[Field(x, y) for x in range(100, 700, 75)] for y in range(100, 700, 75)])
+        #print(mac)
+        self.board = pd.DataFrame(mac, ["a", "b", "c", "d", "e", "f", "g", "h"])
+        #print(self.board)
 
     def run_game(self):
         pg.init()
-        screen = pg.display.set_mode((1200, 800))
-        screen.fill((255, 255, 255))
-        pg.display.set_caption("Pygame")
-        pg.draw.rect(screen, (10, 10, 10), pg.Rect(100, 100, 50, 50))
+        screen_white = pg.display.set_mode((800, 800))
+        screen_white.fill((255, 255, 255))
+        pg.display.set_caption("White")
+        ##pg.draw.rect(screen_white, (10, 10, 10), Field(100, 100))
         while True:
+            for x in range(0,8):
+                for y in range(0,8):
+                    pg.draw.rect(screen_white, (10, 10, 10), self.board.loc[chr(x+ord("a")), y])
+                #print(field)
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     sys.exit()
--- Field.py
+++ Field.py
@@ -1,9 +1,9 @@
 import pygame as pg
 
 
-class Field(pg.rect.RectType):
-    def __init__(self, x, y, width, height):
-        super().__init__(x, y, width, height)
+class Field:
+    def __init__(self, x, y, width=50, height=50):
+        self.rect = pg.Rect(x, y, width, height)
         self.piece = None
 
     def has_piece(self):
@@ -11,3 +11,9 @@ class Field(pg.rect.RectType):
 
     def set_piece(self, piece):
         self.piece = piece
+
+    def get_piece(self):
+        return self.piece
+
+    def get_rect(self):
+        return self.rect
